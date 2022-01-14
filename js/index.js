bf8823b621c45f23d222467d1e89e17beaf3ad9b
nataly garcia <naty_garcioli@hotmail.com>
2022-01-13 19:12:07 -0500

    pausado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 let profileName = document.querySelector("#profileName");
-console.log(profileName)
 
 function changeName(element){
   profileName.innerText = "Naty García"
+}
+
+let acceptInvite = document.querySelectorAll(".user-line.borrar");
+console.log(acceptInvite);
+
+
+for(let i = 0; i < acceptInvite.length; i++) {
+  acceptInvite[i].addEventListener('click', remove , false)
+}
+
+function removeUser(element){
+  acceptInvite.remove();
 }
